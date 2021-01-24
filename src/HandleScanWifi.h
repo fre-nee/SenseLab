2b56e455e9e940c006b3d38876fe1f332b9b9d84
Freddy  Thobhani <thobhani.freddy@gmail.com>
2021-01-24 19:52:49 +0530

    scanwifi endpoint to get scanned network list

--- src/HandleScanWifi.h
+++ src/HandleScanWifi.h
@@ -1,24 +1,28 @@
-void onComplete(int networksFound)
-{
-  DynamicJsonDocument doc(1024);
-  Serial.printf("%d network(s) found\n", networksFound);
-  for (int i = 0; i < networksFound; i++)
-  {
-    DynamicJsonDocument wifi(1024);
-    wifi["ssid"] = WiFi.SSID(i).c_str();
-    wifi["channel"] = WiFi.channel(i);
-    wifi["rssi"] = WiFi.RSSI(i);
-    wifi["open"] = WiFi.encryptionType(i) == ENC_TYPE_NONE;
-    String wifiString;
-    serializeJson(wifi, wifiString);
-    doc[String(i)] = wifiString;
-  }
-  String output;
-  serializeJson(doc, output);
-  server.send(200, "application/json", output);
-}
-
 void handleScanWifi()
 {
-  WiFi.scanNetworks(onComplete);
+  int nNetworks = WiFi.scanNetworks(false, true);
+  DynamicJsonDocument wifiNetworks(1024);
+  Serial.printf("%d network(s) found\n", nNetworks);
+  for (int i = 0; i < nNetworks; i++)
+  {
+    String ssid;
+    uint8_t encryptionType;
+    int32_t RSSI;
+    uint8_t* BSSID;
+    int32_t channel;
+    bool isHidden;
+
+    WiFi.getNetworkInfo(i, ssid, encryptionType, RSSI, BSSID, channel, isHidden);
+    DynamicJsonDocument wifi(1024);
+    wifi["ssid"] = ssid.c_str();
+    wifi["channel"] = channel;
+    wifi["rssi"] = WiFi.RSSI(i);
+    wifi["open"] = encryptionType == ENC_TYPE_NONE;
+    String wifiString;
+    serializeJson(wifi, wifiString);
+    wifiNetworks[String(i)] = wifiString;
+  }
+  String res;
+  serializeJson(wifiNetworks, res);
+  server.send(200, "application/json", res);
 }
